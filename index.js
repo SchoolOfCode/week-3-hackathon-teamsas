a9565d04b2e502ae794b98e7358d782b058f61a5
Sergio <sergeh96@gmail.com>
2024-12-12 16:16:35 +0000

    button functionality and logic progress

--- index.js
+++ index.js
@@ -22,24 +22,42 @@ async function getCorrectDog() {
     console.log(error);
   }
 }
-/* 
+
+async function getDogDescription(dogInfo){
+    let dogDescription = 
+}
+
 async function getIncorrectDogs() {
-  let incorrectDogs = await fetch(
-    `https://api.thedogapi.com/v1/images/search?limit=${limitParam}`,
-    {
-      headers: {
-        "x-api-key":
-          "live_EwtozMG6f4aHbDVWyxT9ELUto8wgIXV74bjePkRZfS9KFgA6ySIrbhI9mUsA4fD3",
-      },
-    }
-  );
+  try {
+    let incorrectDogs = await fetch(
+      `https://api.thedogapi.com/v1/images/search?limit=${limitParam}&has_breeds=${breeds}`,
+      {
+        headers: {
+          "x-api-key":
+            "live_EwtozMG6f4aHbDVWyxT9ELUto8wgIXV74bjePkRZfS9KFgA6ySIrbhI9mUsA4fD3",
+        },
+      }
+    );
 
-  let incorrectDogsData = await incorrectDogs.json();
+    let incorrectDogsData = await incorrectDogs.json();
 
-  incorrectDogsData.forEach((dog, index) => {
-    console.log(`Dog ${index + 1}:`, dog);
-  });
-} */
+    console.log(incorrectDogsData);
+
+    // .map returns a new array
+
+    let dogsInfoArray = incorrectDogsData.map((dog) => {
+      return {
+        name: dog.breeds[0].name,
+      };
+    });
+
+    console.log(dogsInfoArray);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getIncorrectDogs();
 
 document.addEventListener("DOMContentLoaded", () => {
   let startButton = document.getElementById("startGame");
@@ -51,16 +69,25 @@ async function startGame() {
   let correctDogObject = await getCorrectDog();
   console.log(correctDogObject);
 
-  createButtons();
+  createButtons(correctDogObject);
 
   imgToUpdate.src = correctDogObject.img;
 }
 
-async function createButtons() {
-  let correctDogObject = await getCorrectDog();
+async function createButtons(correctDogObject) {
+  let multipleChoiceArray = [];
+
+  // Getting API info for correct dog, pushing it to array
+  let correctDogName = correctDogObject.name;
+  multipleChoiceArray.push(correctDogName);
+  console.log(multipleChoiceArray);
+
+  // Finding start button and removing
   let startButton = document.getElementById("startGame");
   startButton.remove();
 
+  // Creating new buttons
   let newButton = document.createElement("button");
-  newButton.innerText = correctDogObject.name;
+  newButton.innerText = correctDogName;
+  document.querySelector(".buttonList").appendChild(newButton);
 }
